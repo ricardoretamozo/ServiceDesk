e5882fd3841062eaf1ee0b0400c31c1e6b5767e3
ricardoretamozo <ricardo.retamozo.garay@gmail.com>
2022-05-08 14:59:43 -0500

    Configuracion de base de datos.

--- src/main/java/com/cmms/servicedesk/ServiceDeskApplication.java
+++ src/main/java/com/cmms/servicedesk/ServiceDeskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceDeskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDeskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeskApplication.class, args);
+    }
 
 }
--- src/test/java/com/cmms/servicedesk/ServiceDeskApplicationTests.java
+++ src/test/java/com/cmms/servicedesk/ServiceDeskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServiceDeskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
