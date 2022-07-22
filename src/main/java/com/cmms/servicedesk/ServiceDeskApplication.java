28b8a9c8e4101c834a5492b384bfd243d216224f
ricardoretamozo <ricardo.retamozo@tecsup.edu.pe>
2022-07-22 01:17:09 -0500

    Al crear, actualizar e eliminar una persona borramos o creamos el registro en persona organo o estado tecnico(mejorado).

--- src/main/java/com/cmms/servicedesk/ServiceDeskApplication.java
+++ src/main/java/com/cmms/servicedesk/ServiceDeskApplication.java
@@ -30,25 +30,25 @@ public class ServiceDeskApplication {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-/*
+    /*
     @Bean
     CommandLineRunner run(PerfilPersonaService perfilService, PersonaService personaService) {
         return args -> {
 
             PerfilPersona perfil1 = new PerfilPersona(1, "COORDINADOR INFORMATICO", "1", 'S');
-            PerfilPersona perfil2 = new PerfilPersona(2, "ASISTENTE INFORMATICO", "1", 'S');
-            PerfilPersona perfil3 = new PerfilPersona(3, "SOPORTE TECNICO", "1", 'S');
-            PerfilPersona perfil4 = new PerfilPersona(4, "USUARIO COMUN", "1", 'N');
+//            PerfilPersona perfil2 = new PerfilPersona(2, "ASISTENTE INFORMATICO", "1", 'S');
+//            PerfilPersona perfil3 = new PerfilPersona(3, "SOPORTE TECNICO", "1", 'S');
+//            PerfilPersona perfil4 = new PerfilPersona(4, "USUARIO COMUN", "1", 'N');
+//
+//            perfilService.create(perfil1);
+//            perfilService.create(perfil2);
+//            perfilService.create(perfil3);
+//            perfilService.create(perfil4);
 
-            perfilService.create(perfil1);
-            perfilService.create(perfil2);
-            perfilService.create(perfil3);
-            perfilService.create(perfil4);
-
-            personaService.create(new Persona(null, "Juan","apellido","73824465", "73824465", "cocacola", "usuario@gmail.com","942035699",null,'M','A',perfil1));
-            personaService.create(new Persona(null, "Roberto","apellido","83824465","83824465","cocacola","juan@gmail.com","942035690",null,'M','A',perfil2));
-            personaService.create(new Persona(null, "Gabriel","apellido","93824465","93824465","cocacola","carlos@gmail.com","942035691",null,'M','A',perfil3));
-            personaService.create(new Persona(null, "Carlos","apellido","13824465","13824465","cocacola","gabriel@gmail.com","942035693",null,'M','A',perfil4));
+            personaService.update(new Persona(1, "Juan","apellido","73824465", "73824465", "cocacola", "usuario@gmail.com","942035699",null,'M','A',perfil1));
+//            personaService.create(new Persona(null, "Roberto","apellido","83824465","83824465","cocacola","juan@gmail.com","942035690",null,'M','A',perfil2));
+//            personaService.create(new Persona(null, "Gabriel","apellido","93824465","93824465","cocacola","carlos@gmail.com","942035691",null,'M','A',perfil3));
+//            personaService.create(new Persona(null, "Carlos","apellido","13824465","13824465","cocacola","gabriel@gmail.com","942035693",null,'M','A',perfil4));
 
         };
     }*/
